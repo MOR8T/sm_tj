dd53c36626a59310495dd521921d6e0f9e99fc0c
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2025-08-08 14:28:12 +0500

    chante some part

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,8 +2,16 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "tj", "ru"],
+  locales: ["tj", "ru", "en"],
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: "tj",
+
+  // pathnames: {
+  //   "/contact": {
+  //     en: "contact-en",
+  //     ru: "contact-ru",
+  //     tj: "contact-tj",
+  //   },
+  // },
 });
